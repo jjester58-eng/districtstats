1203b627516a2781bc1d451502072cb9381c3e9d
jjester58-eng <jjester58@gmail.com>
2026-04-24 15:49:54 +0000

    fixes

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
     const [roster, setRoster] = useState([]);
     const [games, setGames] = useState([]);
